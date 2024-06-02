e830cbd7c6bc0821d071594aceb21c78b84de98a
Allison Richmond <arichmond79@gmail.com>
2024-06-02 18:15:25 -0500

    add Netlify adapter and change output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: netlify(),
+	output: "server",
 	integrations: [
 		tailwind({
 			// Example: Disable injecting a basic `base.css` import on every page.
 			// Useful if you need to define and/or import your own custom `base.css`.
 			// applyBaseStyles: false,
 		}),
-		react(),
 		partytown(),
+		react(),
 	],
 });
