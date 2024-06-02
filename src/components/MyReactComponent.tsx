487e897c5386a003a7b1ab8fa2b14e15edf0de4d
Allison Richmond <arichmond79@gmail.com>
2024-06-02 00:01:58 -0500

    add multiple MyReactComponent with different client directives; add SetHtml component; add default anchor styles

--- src/components/MyReactComponent.tsx
+++ src/components/MyReactComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export type MyReactComponentProps = React.PropsWithChildren<
@@ -13,16 +13,26 @@ export default function MyReactComponent({
 	className,
 	...props
 }: MyReactComponentProps) {
+	const [count, setCount] = useState(0);
 	return (
 		<div
 			{...props}
-			className={twMerge(
-				"px-4 py-2 my-2 -mx-4 bg-black rounded",
-				className
-			)}
+			className={twMerge("p-4 my-2 -mx-4 bg-black rounded", className)}
 		>
-			<p>My React Component!</p>
-			{children}
+			<div>
+				{/* <p className="text-xs">My React Component?</p> */}
+				{children || <p className="my-0">Cool React Component!</p>}
+			</div>
+			<button
+				className="bg-neutral-100 text-neutral-900 px-2 py-1 rounded my-1"
+				type="button"
+				onClick={() => setCount(count + 1)}
+			>
+				<code>
+					Clicked button <strong>{count}</strong> times.
+				</code>
+			</button>
+			<pre className="text-xs mt-2">{JSON.stringify(props)}</pre>
 		</div>
 	);
 }
