a2c1f59335ca344d0ded5911cffff097dbfabfee
Allison Richmond <arichmond79@gmail.com>
2024-06-03 20:34:34 -0500

    add /product.json?sku={sku} endpoint

---
+++ src/pages/product.json.ts
@@ -0,0 +1,26 @@
+export async function GET({ params, request, site, url }) {
+	const model = url.searchParams.get("model");
+	const sku = url.searchParams.get("sku");
+
+	const bannerDomain = "https://www.uat2.origin.footlocker.com";
+	const baseRoute = `${bannerDomain}/zgw/product-core/v1/pdp/`;
+	const route = sku
+		? `${baseRoute}/sku/${sku}`
+		: `${baseRoute}/model/${model}`;
+
+	const product = await fetch(route).then((r) => r.json());
+
+	if (!product) {
+		return new Response(null, {
+			status: 404,
+			statusText: "Not found",
+		});
+	}
+
+	return new Response(JSON.stringify(product), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+}
