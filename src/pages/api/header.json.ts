2ee435cf39dcb333c63104be8c092ce941543fcc
Allison Richmond <arichmond79@gmail.com>
2024-07-07 19:59:54 -0500

    update HeaderResponse types, catch fetch error

--- src/pages/api/header.json.ts
+++ src/pages/api/header.json.ts
@@ -1,6 +1,50 @@
 import type { APIContext } from "astro";
 import { getBannerDomain } from "@utils/banner.configs";
 
+export interface HeaderResponse {
+	header: Header[];
+}
+
+export interface Header {
+	headerSection?: HeaderSection[];
+	headerLinks?: HeaderLink[];
+}
+
+export interface HeaderLink {
+	name: string;
+	linkPath: string;
+}
+
+export interface HeaderSection {
+	name: string;
+	categories: Category[];
+	linkPath?: string;
+}
+
+export interface Category {
+	"name": string;
+	"hidden": boolean;
+	"expandByDefault"?: boolean;
+	"type": "icons" | "list" | "promoLinks";
+	"links": Link[];
+	"sub-categories": unknown[];
+	"style"?: string;
+	"linkPath"?: string;
+}
+
+export interface Link {
+	shopAll?: boolean;
+	text?: string;
+	url: string;
+	icon?: Icon;
+	image?: string;
+}
+
+export interface Icon {
+	name: string;
+	url: string;
+}
+
 export async function GET(ctx: APIContext) {
 	const bannerDomain = getBannerDomain(ctx);
 	const lang = ctx.currentLocale || "en";
@@ -8,10 +52,16 @@ export async function GET(ctx: APIContext) {
 	const route = `${bannerDomain}/api/content/${lang}/header.details.json`;
 
 	const headers: HeadersInit = { "x-api-lang": locale };
+	const init: RequestInit = { headers };
+	let resp;
+	try {
+		resp = await fetch(route, init).then((r) => r.json());
+	} catch (err) {
+		console.error(`Error with route ${route}:`, err);
+	}
 
-	const results = await fetch(route, { headers }).then((r) => r.json());
-	const header = results?.header;
-	// console.log("HeaderNav results.header:", header);
+	const header = resp?.header as Header;
+	// console.log("resp.header:", header);
 
 	if (!header || !Array.isArray(header)) {
 		return new Response(null, {
@@ -21,9 +71,9 @@ export async function GET(ctx: APIContext) {
 	}
 
 	const initial = {
-		components: [] as any[],
-		links: [] as any[],
-		sections: [] as any[],
+		components: [] as unknown[],
+		links: [] as HeaderLink[],
+		sections: [] as HeaderSection[],
 	};
 
 	const data = header.reduce((all, item, i) => {
@@ -38,8 +88,6 @@ export async function GET(ctx: APIContext) {
 		}
 		return all;
 	}, initial);
-
-	// console.log("data.sections", data.sections);
 
 	return new Response(JSON.stringify(data), {
 		status: 200,
