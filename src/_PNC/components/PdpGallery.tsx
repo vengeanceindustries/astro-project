d7a7b6780090a6a1d73bf72406d749add85c757c
Allison Richmond <arichmond79@gmail.com>
2024-08-18 18:11:04 -0500

    add separate PdpGallery component, move ProductImage under PNC/components

---
+++ src/_PNC/components/PdpGallery.tsx
@@ -0,0 +1,50 @@
+import { ProductImage, type ProductDetailsFormatted } from "@PNC/components";
+import clsx from "clsx";
+
+const imgWidth = 550;
+
+export function PdpGallery({
+	model,
+	style,
+}: Pick<ProductDetailsFormatted, "model" | "style">) {
+	const alt = `${model.name} - ${model.gender} - ${style.color}`;
+	const variants = style.imageUrl?.variants;
+
+	const masonry = variants && (
+		<ul
+			className={clsx(
+				"hidden md:grid",
+				"auto-rows-fr gap-4",
+				"grid-cols-2 lg:grid-cols-3 xl:grid-rows-[masonry]"
+			)}
+		>
+			{variants.map((variant, i) => (
+				<li
+					key={variant}
+					className={clsx(
+						{ "col-span-2 row-span-2": i === 0 },
+						"bg-white"
+					)}
+				>
+					<ProductImage
+						alt={`${alt} - ${i + 1}`}
+						src={variant}
+						width={imgWidth}
+					/>
+				</li>
+			))}
+		</ul>
+	);
+	return (
+		<div data-id="gallery">
+			{masonry}
+			<ProductImage
+				alt={alt}
+				className="md:hidden"
+				sku={style.sku}
+				src={style.imageUrl?.base}
+				width={imgWidth}
+			/>
+		</div>
+	);
+}
--- src/_PNC/components/index.tsx
+++ src/_PNC/components/index.tsx
@@ -1,18 +1,14 @@
-import React from "react";
-import ProductImage from "@components/ProductImage";
-import type { Sku, StyleVariant } from "@PNC/layouts/ProductDetails";
 import clsx from "clsx";
+import ProductImage from "@PNC/components/ProductImage";
+import type { Sku, StyleVariant } from "@PNC/layouts/ProductDetails";
 import {
+	type FormattedPdpSize,
 	type ProductDetailsFormatted,
 	type formatPrice,
 } from "@PNC/components/utils";
-export {
-	type FormattedPdpSize,
-	type ProductDetailsFormatted,
-} from "@PNC/components/utils";
+export { type FormattedPdpSize, type ProductDetailsFormatted, ProductImage };
+export { PdpGallery } from "@PNC/components/PdpGallery";
 export { PdpSizes } from "@PNC/components/PdpSizes";
-
-const imgWidth = 550;
 
 export function PdpHeader({
 	className,
@@ -31,52 +27,6 @@ export function PdpHeader({
 				</a>
 			</p>
 		</header>
-	);
-}
-
-export function PdpGallery({
-	model,
-	style,
-}: Pick<ProductDetailsFormatted, "model" | "style">) {
-	const alt = `${model.name} - ${model.gender} - ${style.color}`;
-	const variants = style.imageUrl?.variants;
-
-	const masonry = variants && (
-		<ul
-			className={clsx(
-				"hidden md:grid",
-				"auto-rows-fr gap-4",
-				"grid-cols-2 lg:grid-cols-3 xl:grid-rows-[masonry]"
-			)}
-		>
-			{variants.map((variant, i) => (
-				<li
-					key={variant}
-					className={clsx(
-						{ "col-span-2 row-span-2": i === 0 },
-						"bg-white"
-					)}
-				>
-					<ProductImage
-						alt={`${alt} - ${i + 1}`}
-						src={variant}
-						width={imgWidth}
-					/>
-				</li>
-			))}
-		</ul>
-	);
-	return (
-		<div data-id="gallery">
-			{masonry}
-			<ProductImage
-				alt={alt}
-				className="md:hidden"
-				sku={style.sku}
-				src={style.imageUrl?.base}
-				width={imgWidth}
-			/>
-		</div>
 	);
 }
 
